dc47b87919179d36456e47091c99406f29ba1433
bofrank <bo@bofrank.com>
2025-10-26 20:30:19 +0900

    Adding refs.

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
+ 
 export default function Workout({ title, description, time, onComplete }) {
+  const timer = React.useRef();
+ 
   function handleStartWorkout() {
-    // Todo: Start timer
+    timer.current = setTimeout(handleStopWorkout, time);
   }
-
+ 
   function handleStopWorkout() {
-    // Todo: Stop timer
+    clearTimeout(timer.current);
     onComplete();
   }
-
+ 
   return (
     <article className="workout">
       <h3>{title}</h3>
